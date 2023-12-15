8c0815b362867db0b43dd9cfec6dd5723e670390
Alex Szabo <delanni.alex@gmail.com>
2023-12-15 23:44:05 +0100

    fix: fix bad export

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
-/* generated */ const version = { "date": "2023-12-15T22:41:29.101Z", "commit": "try to bust the cache on version.js" }
+/* generated */ const version = { "date": "2023-12-15T22:45:26.499Z", "commit": "fix: fix bad export" }
 
 console.log("VERSION", version);
