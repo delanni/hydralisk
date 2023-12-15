6f95cb7d36b4ae603af72cc3f33c7c7fa1343636
Alex Szabo <delanni.alex@gmail.com>
2023-12-15 23:40:41 +0100

    try to bust the cache on version.js

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
-/* generated */ const version = { "date": "2023-12-15T22:35:58.309Z", "commit": "fix: use object values instead of entries" }
+/* generated */ const version = { "date": "2023-12-15T22:41:29.101Z", "commit": "try to bust the cache on version.js" }
 
 console.log("VERSION", version);
