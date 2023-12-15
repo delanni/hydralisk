e536a8367b4c910237ef708789abbfe5d8cd9a24
Alex Szabo <delanni.alex@gmail.com>
2023-12-15 23:50:44 +0100

    try fixing the pre-push hook

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
-/* generated */ const version = { "date": "2023-12-15T22:45:26.499Z", "commit": "fix: fix bad export" }
+/* generated */ const version = { "date": "2023-12-15T22:50:47.894Z", "commit": "try fixing the pre-push hook" }
 
 console.log("VERSION", version);
