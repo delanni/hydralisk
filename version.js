5c7f028fc542d3cee945a90e8c4a9d49362ada79
Alex Szabo <delanni.alex@gmail.com>
2025-05-30 23:51:53 +0200

    add gitignore

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
-/* generated */ const version = { "date": "2023-12-15T23:14:34.907Z", "commit": "try running node from sh, to see if it's respecting the pre-push" }
+/* generated */ const version = { "date": "2025-05-30T21:56:04.662Z", "commit": "add gitignore" }
 
 console.log("VERSION", version);
