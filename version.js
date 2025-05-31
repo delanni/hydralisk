26ef74c669ca48f94995d59ad564664756a7ec9f
Alex Szabo <delanni.alex@gmail.com>
2025-05-31 20:54:10 +0200

    update sketches.json - fix sketch loading

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
-/* generated */ const version = { "date": "2025-05-31T18:45:22.646Z", "commit": "keep only filters locally", "sha": "baf6a97519146efbaf3bd26570a688aad25621d2" };
+/* generated */ const version = { "date": "2025-05-31T18:54:13.830Z", "commit": "update sketches.json - fix sketch loading", "sha": "2973fe98426575a9ff9c1543a6494a6c4f41040f" };
 
 console.log("VERSION", version);
